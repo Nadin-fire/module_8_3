866f832de1f0374de4553b32d212a89892d11f64
Nadin-fire <ioyanvm@gmail.com>
2024-12-14 01:41:14 +0800

    Add files via upload

--- module_8_3.py
+++ module_8_3.py
@@ -11,10 +11,10 @@ class IncorrectCarNumbers(Exception):
 class Car:
     def __init__(self, model: str, vin: int, numbers: str):
         self.model = model
+        self.__is_valid_vin(vin)
+        self.__is_valid_numbers(numbers)
         self.__vin = vin
         self.__numbers = numbers
-        self.__is_valid_vin(self.__vin)
-        self.__is_valid_numbers(self.__numbers)
 
 
     @staticmethod
